ddd788e655e41a75c4cc6798462e6ac2de59cb3f
Luzin7 <lvictor.dutra@gmail.com>
2024-08-31 23:14:16 -0300

    feat: add logic to open explorer and upload file (not done yet)

--- UI/MainWindow.cs
+++ UI/MainWindow.cs
@@ -1,26 +1,62 @@
 using Gtk;
 
-public class MainWindow : Window
+namespace DropUp.UI
 {
-    private readonly FileUploaderService _fileUploaderService;
-
-    public MainWindow(FileUploaderService fileUploaderService) : base("DropUp")
+    public class MainWindow : Window
     {
-        _fileUploaderService = fileUploaderService;
+        private readonly UploadFileService _uploadFileService;
 
-        SetDefaultSize(400, 200);
-        DeleteEvent += (o, e) => Application.Quit();
+        public MainWindow(UploadFileService uploadFileService) : base("DropUp")
+        {
+            _uploadFileService = uploadFileService;
 
-        var button = new Button("Upload File");
-        button.Clicked += OnUploadButtonClicked;
+            SetDefaultSize(400, 200);
+            DeleteEvent += (o, e) => Application.Quit();
 
-        Add(button);
-        ShowAll();
-    }
+            var button = new Button("Upload File");
 
-    private async void OnUploadButtonClicked(object sender, EventArgs e)
-    {
-        Console.WriteLine("Upload de arquivo iniciado...");
-        // Aqui você pode adicionar a lógica de upload
+            button.Clicked += OnUploadButtonClicked;
+
+            Add(button);
+            ShowAll();
+        }
+
+        private void OnUploadButtonClicked(object? sender, EventArgs e)
+        {
+            using var fileChooser = new FileChooserDialog(
+                 "Select a file",
+                 this,
+                 FileChooserAction.Open,
+                 "Cancel", ResponseType.Cancel,
+                 "Open", ResponseType.Accept);
+            if (fileChooser.Run() == (int)ResponseType.Accept)
+            {
+                string filePath = fileChooser.Filename;
+                UploadFile(filePath);
+            }
+            fileChooser.Destroy();
+        }
+
+        private void UploadFile(string filePath)
+        {
+            Console.WriteLine(filePath);
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"File not found: {filePath}");
+                return;
+            }
+
+            try
+            {
+                using var fileStream = File.OpenRead(filePath);
+                string fileName = System.IO.Path.GetFileName(filePath);
+                _uploadFileService.Execute(fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
     }
 }
