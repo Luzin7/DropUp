d3b69a38414fcbd039fccbdc77b6aa083f56257e
Luzin7 <lvictor.dutra@gmail.com>
2024-09-23 19:08:54 -0300

    refactor: add download url

--- Modules/FileUploader/Repositories/Implementations/FileUploaderRepositoryImplementation.cs
+++ Modules/FileUploader/Repositories/Implementations/FileUploaderRepositoryImplementation.cs
@@ -10,7 +10,10 @@ public class FileUploaderRepositoryImplementation(IAmazonS3 s3Client, S3Config _
         var fileName = Path.GetFileName(filePath);
 
         await fileTransferUtility.UploadAsync(filePath, _config.BucketName, fileName);
-        Console.WriteLine($"Arquivo enviado com sucesso para: https://{_config.BucketName}.{_config.Endpoint}/{fileName}");
-        return $"https://{_config.BucketName}.{_config.Endpoint}/{fileName}";
+
+        string downloadUrl = s3Client.GeneratePreSignedURL(_config.BucketName, fileName, DateTime.UtcNow.Add(TimeSpan.FromMinutes(1)), null);
+        Console.WriteLine($"Arquivo enviado com sucesso para: {downloadUrl}");
+        return $"{downloadUrl}";
+
     }
 }
